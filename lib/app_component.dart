f96da6c7160f5bc38b5d2f8b391f8a9f95856f8a
Patrice Chalin <pchalin@gmail.com>
2017-02-08 09:20:45 -0800

    Sync with 0d80b33
    Synced with dart-lang/site-webdev chalin-ng-router-1221 branch, commit 0d80b33:
    https://github.com/chalin/site-webdev/tree/0d80b33be54130519ceff5b78d64b3bc5e1998d9/public/docs/_examples/router/dart

---
+++ lib/app_component.dart
@@ -0,0 +1,42 @@
+// This is a copy of app_component_4.dart
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+// Not yet used: import 'compose_message_component.dart';
+import 'crisis_center/crisis_center_component.dart';
+import 'heroes/hero_detail_component.dart';
+import 'heroes/hero_service.dart';
+import 'heroes/heroes_component.dart';
+import 'not_found_component.dart';
+
+@Component(
+    selector: 'my-app',
+    template: '''
+      <h1>Angular Router</h1>
+      <nav>
+        <a [routerLink]="['CrisisCenter']">Crisis Center</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+        <!--
+        <a [routerLink]="['CrisisCenter', 'CrisisDetail', {'id': '1'}]">Dragon Crisis</a>
+        -->
+      </nav>
+      <router-outlet></router-outlet>
+      <!-- Not yet used:
+      <router-outlet name="popup"></router-outlet>
+      -->
+    ''',
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [HeroService, ROUTER_PROVIDERS])
+@RouteConfig(const [
+  const Redirect(path: '/', redirectTo: const ['CrisisCenter']),
+  const Route(
+      path: '/crisis-center/...',
+      name: 'CrisisCenter',
+      component: CrisisCenterComponent),
+  const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent),
+  const Route(
+      path: '/hero/:id', name: 'HeroDetail', component: HeroDetailComponent),
+  // Not yet used: const AuxRoute(path: '/contact', name: 'Contact', component: ComposeMessageComponent),
+  const Route(path: '/**', name: 'NotFound', component: NotFoundComponent)
+])
+class AppComponent {}
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/platform/browser.dart';
 
-import 'package:router_example/app_component_4.dart';
+import 'package:router_example/app_component.dart';
 
 void main() {
   bootstrap(AppComponent);
