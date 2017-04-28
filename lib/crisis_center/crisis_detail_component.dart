6367a856a2506aa990aac6d21466cca83a1f3e5d
Patrice Chalin <pchalin@gmail.com>
2017-04-28 16:35:48 -0400

    Sync with c7ca144
    Synced with dart-lang/site-webdev master branch, commit c7ca144:
    https://github.com/dart-lang/site-webdev/tree/c7ca144016839f1ece2ec135ad98e0d98e56b568/examples/ng/doc/router

--- lib/crisis_center/crisis_detail_component.dart
+++ lib/crisis_center/crisis_detail_component.dart
@@ -11,7 +11,14 @@ import 'dialog_service.dart';
     selector: 'my-crisis-detail',
     templateUrl: 'crisis_detail_component.html',
     styleUrls: const ['crisis_detail_component.css'])
-class CrisisDetailComponent implements CanDeactivate, OnInit {
+class CrisisDetailComponent
+    implements
+        CanDeactivate,
+        CanReuse,
+        OnActivate,
+        OnDeactivate,
+        OnInit,
+        OnReuse {
   Crisis crisis;
   String name;
   final CrisisService _crisisService;
@@ -22,9 +29,10 @@ class CrisisDetailComponent implements CanDeactivate, OnInit {
   CrisisDetailComponent(this._crisisService, this._router, this._routeParams,
       this._dialogService);
 
-  Future<Null> ngOnInit() async {
-    var _id = _routeParams.get('id');
-    var id = int.parse(_id ?? '', onError: (_) => null);
+  Future<Null> ngOnInit() => _setCrisis(_routeParams.get('id'));
+
+  Future<Null> _setCrisis(String idAsString) async {
+    var id = int.parse(idAsString ?? '', onError: (_) => null);
     if (id != null) crisis = await (_crisisService.getCrisis(id));
     if (crisis != null) name = crisis.name;
   }
@@ -39,9 +47,27 @@ class CrisisDetailComponent implements CanDeactivate, OnInit {
         crisis == null ? {} : {'id': crisis.id.toString()}
       ]);
 
+  // TODO: remove cast of true once there is a fix for https://github.com/dart-lang/sdk/issues/25368
   @override
-  /*FutureOr<bool>*/ routerCanDeactivate(next, prev) =>
+  FutureOr<bool> routerCanDeactivate(next, prev) =>
       crisis == null || crisis.name == name
-          ? true
+          ? true as FutureOr<bool>
           : _dialogService.confirm('Discard changes?');
+
+  @override
+  FutureOr<bool> routerCanReuse(next, prev) => true;
+
+  @override
+  Future<Null> routerOnReuse(ComponentInstruction next, prev) =>
+      _setCrisis(next.params['id']);
+
+  @override
+  void routerOnActivate(next, prev) {
+    print('Activating ${next.routeName} ${next.urlPath}');
+  }
+
+  @override
+  void routerOnDeactivate(next, prev) {
+    print('Deactivating ${prev.routeName} ${prev.urlPath}');
+  }
 }
--- lib/not_found_component.dart
+++ lib/not_found_component.dart
@@ -1,8 +1,4 @@
 import 'package:angular2/core.dart';
 
-@Component(
-    // selector isn't needed, but must be provided
-    // https://github.com/dart-lang/angular2/issues/60
-    selector: 'my-not-found',
-    template: '<h2>Page not found</h2>')
+@Component(selector: 'my-not-found', template: '<h2>Page not found</h2>')
 class NotFoundComponent {}
