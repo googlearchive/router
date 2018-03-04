179a58de058e96e61d2eade2459b65133370a0c5
Patrice Chalin <pchalin@gmail.com>
2018-03-04 17:34:13 -0500

    Sync with ff1d66f
    Synced with dart-lang/site-webdev 5-dev branch, commit ff1d66f:
    https://github.com/dart-lang/site-webdev/tree/ff1d66f83499f99af0fa31747188db606cd13c97/examples/ng/doc/router

--- lib/src/crisis_center/crises_component.dart
+++ lib/src/crisis_center/crises_component.dart
@@ -12,7 +12,7 @@ import 'crisis_center_home_component.dart';
   selector: 'my-crises',
   templateUrl: 'crises_component.html',
   styleUrls: const ['crises_component.css'],
-  directives: const [CORE_DIRECTIVES, ROUTER_DIRECTIVES],
+  directives: const [coreDirectives, ROUTER_DIRECTIVES],
 )
 @RouteConfig(const [
   const Route(
