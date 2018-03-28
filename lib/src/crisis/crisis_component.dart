17c1514616cf4a25301f86a0449035739374e84f
Patrice Chalin <pchalin@gmail.com>
2018-03-28 11:27:36 -0400

    Sync with 08d6f28
    Synced with dart-lang/site-webdev 5-dev branch, commit 08d6f28:
    https://github.com/dart-lang/site-webdev/tree/08d6f288a4fedbeb291875ef8b9e78ee2fbf6fa8/examples/ng/doc/router

--- lib/src/crisis/crisis_component.dart
+++ lib/src/crisis/crisis_component.dart
@@ -4,10 +4,11 @@ import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 import 'package:angular_router/angular_router.dart';
 
-import 'route_paths.dart' as paths;
+import '../instance_logger.dart';
 import 'crisis.dart';
 import 'crisis_service.dart';
 import 'dialog_service.dart';
+import 'route_paths.dart' as paths;
 
 @Component(
   selector: 'my-crisis',
@@ -15,53 +16,40 @@ import 'dialog_service.dart';
   styleUrls: ['crisis_component.css'],
   directives: [coreDirectives, formDirectives],
 )
-class CrisisComponent // extends Object with CanReuse
-    implements
-        CanDeactivate,
-        OnActivate,
-        OnDeactivate {
-  static int _instanceCount = 0; // FIXME: temporary
-  final instanceId = _instanceCount++; // FIXME: temporary
+class CrisisComponent extends Object
+    with CanReuse, InstanceLogger
+    implements CanDeactivate, OnActivate, OnDeactivate {
   Crisis crisis;
-  // String name; // FIXME: temporarily splitting into getter/setter
-  String _name;
-  void set name(String n) {
-    _name = n;
-    print('>> [$instanceId] Crisis name changed to $_name');
-  }
-
-  String get name => _name;
-
+  String name;
   final CrisisService _crisisService;
   final Router _router;
   final DialogService _dialogService;
+  String get loggerPrefix => 'CrisisComponent';
 
   CrisisComponent(this._crisisService, this._router, this._dialogService) {
-    print('[$instanceId] CrisisComponent created');
+    log('created');
+  }
+
+  @override
+  Future<void> onActivate(_, RouterState current) async {
+    log('onActivate: ${_?.toUrl()} -> ${current?.toUrl()}');
+    final id = _getId(current);
+    if (id == null) return null;
+    crisis = await (_crisisService.get(id));
+    name = crisis?.name;
+    log('onActivate: set name = $name');
+  }
+
+  @override
+  void onDeactivate(RouterState current, _) {
+    log('onDeactivate: ${current?.toUrl()} -> ${_?.toUrl()}');
   }
 
   int _getId(RouterState routerState) => int
       .parse(routerState.parameters[paths.idParam] ?? '', onError: (_) => null);
 
-  @override
-  Future<void> onActivate(_, RouterState current) async {
-    print(
-        '>> [$instanceId] Crisis onActivate ${_?.toUrl()} ($name) -> ${current?.toUrl()} ...');
-    final id = _getId(current);
-    if (id == null) return null;
-    crisis = await (_crisisService.get(id));
-    name = crisis?.name;
-    print('>> [$instanceId] Crisis onActivate name = $name');
-  }
-
-  @override
-  void onDeactivate(RouterState prev, RouterState current) {
-    print(
-        '>> [$instanceId] Crisis onDeactivate ${prev?.toUrl()} ($name) -> ${current?.toUrl()}');
-  }
-
   Future<void> save() async {
-    print('>> [$instanceId] Crisis save $name (was ${crisis?.name}');
+    log('save: $name (was ${crisis?.name}');
     crisis?.name = name;
     goBack();
   }
@@ -70,13 +58,9 @@ class CrisisComponent // extends Object with CanReuse
 
   @override
   Future<bool> canDeactivate(RouterState prev, RouterState next) async {
-    print(
-        '>> [$instanceId] Crisis canDeactivate ${prev?.toUrl()} -> ${next?.toUrl()}; ${crisis?.name} == $name');
+    log('canDeactivate: ${prev?.toUrl()} -> ${next?.toUrl()}; ${crisis?.name} == $name?');
     return crisis == null || crisis?.name == name
         ? true
         : _dialogService.confirm('Discard changes?');
   }
-
-  //  @override
-  //  Future<bool> canReuse(_, __) async => true;
 }
