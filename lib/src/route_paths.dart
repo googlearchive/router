6795b4bd1f5ce7b09cecea6efa2da1040feb0343
Patrice Chalin <pchalin@gmail.com>
2018-04-16 12:38:36 -0400

    Sync with b9d46aa
    Synced with dart-lang/site-webdev master branch, commit b9d46aa:
    https://github.com/dart-lang/site-webdev/tree/b9d46aaae14e72f04838ec1bd9ebca6b9ebd2865/examples/ng/doc/router

--- lib/src/route_paths.dart
+++ lib/src/route_paths.dart
@@ -4,3 +4,8 @@ final crises = new RoutePath(path: 'crises');
 final heroes = new RoutePath(path: 'heroes');
 final idParam = 'id';
 final hero = new RoutePath(path: '${heroes.path}/:$idParam');
+
+int getId(Map<String, String> parameters) {
+  final id = parameters[idParam];
+  return id == null ? null : int.tryParse(id);
+}
